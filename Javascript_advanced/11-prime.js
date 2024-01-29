7b7dc45f7c6f746e3087fb43d27a40af99b23785
Vaal34 <5619@holbertonstudents.com>
2024-01-29 21:31:00 +0100

    setTimeout

---
+++ Javascript_advanced/11-prime.js
@@ -0,0 +1,37 @@
+const {performance} = require('perf_hooks');
+
+function countPrimeNumbers() {
+
+    let count = 0;
+    let primeNumber;
+
+    for(let i = 2; i <= 100; i++) {
+        primeNumber = true;
+        for(let diviseur = 2; diviseur < i; diviseur++) {
+            if(i % diviseur === 0) { // si cette condition est valider alors le nombre n'est pas premier
+                primeNumber = false;
+                break;
+            };
+        };
+        if(primeNumber) {
+            count += 1;
+        }
+    }
+    return count;
+}
+
+
+function hundredTimes() {
+    for(let countusefonction = 0; countusefonction <= 100; countusefonction++) {
+        countPrimeNumbers();
+    };
+}
+
+const t0 = performance.now();
+
+setTimeout(hundredTimes, 0)
+
+const t1 = performance.now();
+
+const elapsedTime = t1 - t0;
+console.log(`Execution time of printing countPrimeNumbers was ${elapsedTime} milliseconds.`);
