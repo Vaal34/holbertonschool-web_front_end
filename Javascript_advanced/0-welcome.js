30bd350af5d5fd08c3020929f5fd5635567598f3
Vaal34 <5619@holbertonstudents.com>
2024-01-29 11:36:46 +0100

    aleert popup

---
+++ Javascript_advanced/0-welcome.js
@@ -0,0 +1,5 @@
+function welcome(firstName, lastName) {
+    let fullName = firstName + " " + lastName;
+        function displayFullName(fullName) { alert(`Welcome ${fullName}`) }
+    displayFullName(fullName)
+}
